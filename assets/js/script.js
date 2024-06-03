e39e5e23d73351798adebea3796ee98fa5005d57
Neochees <113420296+Neochees@users.noreply.github.com>
2024-06-03 11:09:54 +0200

    Image, question, and options.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 console.log("Hoi");
+const random_factor = Math.floor(Math.random() * 700);
+
+const gooditems = [664, 734, 767, 833, 851, 913, 940, 1099, 1124, 1347, 1382, 619, 638, 654, 731, 759, 815, 823, 918, 921];
+
+getData("http://localhost:3000/questions/"+ getRandomQuestion(gooditems))
 
 fetchData();
 
+function getRandomQuestion(arr){
+    const randomIndex =  Math.floor(Math.random() * arr.length)
+    return arr[randomIndex];
+}
 async function fetchData() {
     try {
         const url = "http://localhost:3000/questions";
@@ -23,5 +32,16 @@ function find_item(data) { };
 function build(data) {
     random_factor = Math.floor(Math.random() * data.length);
     document.getElementById('questionImage').src = "./assets/img/" + data[random_factor]["image"]
-    document.getElementById('question').src = "./assets/json/db.json" + data[random_factor]
+    fetch("http://localhost:3000/questions").then(
+
+    )
 };
+
+async function getData(url) {
+    const response = await fetch(url);
+    const question = await response.json();
+    console.log(question)
+    document.getElementById("text").innerHTML = question.question;
+    document.getElementById("answer").innerHTML = question.options;
+    document.getElementById("questionImage").src = "./assets/img/"+question.image;
+  }
