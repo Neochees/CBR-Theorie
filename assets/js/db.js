a1fa80824faad70562c2bd2700363b5f087674eb
Neochees <113420296+Neochees@users.noreply.github.com>
2024-06-06 13:53:30 +0200

    Quiz question fixed, hardcode and wtill working on the images

--- assets/js/db.js
+++ assets/js/db.js
@@ -20,20 +20,41 @@ async function fetchData() {
         }
         const data = await response.json();
         // console.log('werkt');
-        // find_item(data);
+        find_item(data);
         build(data);
     } catch (error) {
         console.error("Error", error);
     }
 }
 
+function checkFileExists(urlToFile) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("HEAD", urlToFile, false);
+    xhr.send();
+    if (xhr.status == "404") {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 function build(data) {
     random_factor = Math.floor(Math.random() * data.length);
-    document.getElementById('questionImage').src = "./assets/img/" + data[random_factor]["image"]
-    fetch("http://localhost:3000/questions").then(
-
-    )
+    document.getElementById('questionImage').src = "./assets/img/" + data[random_factor]["image"];
+    document.getElementById("img").src = "assets/img/" + type[random_factor]["image"];
+    document.getElementById("question").innerHTML = type[random_factor]["question"];
+    document.cookie = "id=" + type[random_factor]["id"];
 };
+
+function find_item(data) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i]["type"] == 2) {
+            type.push(data[i]);
+        }
+    }
+
+    return true;
+}
 
 async function getData(url) {
     const response = await fetch(url);
--- js2/questions.js
+++ js2/questions.js
@@ -1,8 +1,41 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
+  {
     numb: 1,
-    question: "Werkt deze quiz?",
+    // image: "122.jpg",
+    question: "De lading steekt meer dan 1 meter uit en is gemarkeerd met rode vlaggetjes. Mag dit?",
+    answer: "Ja",
+    options: [
+      "Ja",
+      "Nee",
+    ]
+  },
+  {
+    numb: 2,
+    // image: "246.png",
+    question: "Moeten taxichauffeurs ook een autogordel dragen?",
+    answer: "Ja",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 3,
+    // img: "efvkr_039.jpg",
+    question: "Is anticiperen over het algemeen voordelig voor het brandstofverbruik?",
+    answer: "Ja",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 4,
+    // img: "efvkr_099.jpg",
+    question: "Kun je de remafstand verkorten door te anticiperen?",
     answer: "Nee",
     options: [
       "Ja",
@@ -10,62 +43,181 @@ let questions = [
 
     ]
   },
-    {
-    numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
-    options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
-    ]
-  },
-    {
-    numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
-    options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
-    ]
-  },
-    {
-    numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
-  },
-    {
+  {
     numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
+    // img: "C08.png",
+    question: "Mag een brommobiel hier inrijden?",
+    answer: "Ja",
     options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 6,
+    // img: "320.jpg",
+    question: "Je wilt de weg volgen naar links. Moet je het voertuig aan de rechterkant voor laten gaan?",
+    answer: "Ja",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 7,
+    // img: "348.jpg",
+    question: "Je gaat rechtdoor. Moet je voorrang verlenen aan de fietser?",
+    answer: "Nee",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 8,
+    // img: "336.jpg",
+    question: "Je wilt nog rechts afslaan, mag dat?",
+    answer: "Nee",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 9,
+    // img: "382.jpg",
+    question: "De motorfiets haalt hier in. Is dat veilig?",
+    answer: "Nee",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 10,
+    // img: "153.jpg",
+    question: "Wordt dagrijlicht gebruikt in combinatie met andere lichten?",
+    answer: "Nee",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 11,
+    // img: "efvkr_051.jpg",
+    question: "Een doorgetrokken streep bevindt zich aan jouw kant, aan de andere kant is een onderbroken streep. Mag je nu inhalen?",
+    answer: "Nee",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 12,
+    // img: "2d_007.png",
+    question: "Is het verstandig om de tenaamstellingscode in het dashboardkastje op te bergen?",
+    answer: "Nee",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 13,
+    // img: "2d_002.png",
+    question: "Je hebt je rijbewijs gehaald op je 17e en wilt gaan rijden met een vriend die ook 17 is. Hij heeft ook al het rijbewijs B. Mag je zo gaan rijden?",
+    answer: "Nee",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 14,
+    // img: "123.jpg",
+    question: "De lading steekt meer dan 1 meter uit en is gemarkeerd met rode vlaggetjes. Mag dit?",
+    answer: "Nee",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 15,
+    // img: "ef_001ai.jpg",
+    question: "Mag een kind een heupgordel dragen?",
+    answer: "Nee",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 16,
+    // img: "ef_175.JPG",
+    question: "Als je meer dan een minuut moet wachten in de auto, schakel je dan de motor uit?",
+    answer: "Ja",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 17,
+    // img: "ef_205.JPG",
+    question: "De spanning op de banden is niet correct. Is er nu een grotere kans op slipgevaar?",
+    answer: "Ja",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 18,
+    // img: "efvkr_103.jpg",
+    question: "Is defensief rijgedrag gewenst?",
+    answer: "Ja",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 19,
+    // img: "294.jpg",
+    question: "Mag je 2 meter voor een kruispunt parkeren?",
+    answer: "Nee",
+    options: [
+      "Ja",
+      "Nee",
+
+    ]
+  },
+  {
+    numb: 20,
+    // img: "063.jpg",
+    question: "Je wilt naar links afslaan. Moet je voorrang verlenen aan de fietser?",
+    answer: "Ja",
+    options: [
+      "Ja",
+      "Nee",
+
     ]
   }
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
 ];
